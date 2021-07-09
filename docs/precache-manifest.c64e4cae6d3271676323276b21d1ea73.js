d5402ff2caff47807f1fb0834cfb2853254fec9b
k_houzouji <k_houzouji@sdcns.co.jp>
2021-07-09 09:43:38 +0900

    add docs

---
+++ docs/precache-manifest.c64e4cae6d3271676323276b21d1ea73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f284bd04286e6a0923f",
+    "url": "/vuetest02/css/chunk-vendors.ee2eade7.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/vuetest02/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "7dc5467db99b3dc543fe9c436d4c1a3b",
+    "url": "/vuetest02/index.html"
+  },
+  {
+    "revision": "34c4fca99c4df355ca00",
+    "url": "/vuetest02/js/app.4c80ecd7.js"
+  },
+  {
+    "revision": "2f284bd04286e6a0923f",
+    "url": "/vuetest02/js/chunk-vendors.8683a97c.js"
+  },
+  {
+    "revision": "cbc672a58000c061ff02b5dd5887541f",
+    "url": "/vuetest02/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetest02/robots.txt"
+  }
+]);
